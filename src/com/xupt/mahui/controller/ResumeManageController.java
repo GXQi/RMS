9282f493fc9ed244d7da0d9c2677de4911c559b3
ismahui <ismahui@163.com>
2017-06-03 12:38:37 +0800

    update frame

---
+++ src/com/xupt/mahui/controller/ResumeManageController.java
@@ -0,0 +1,9 @@
+package com.xupt.mahui.controller;
+/**
+ * 简历管理相关的Controller
+ * @author mahui
+ *
+ */
+public class ResumeManageController {
+	
+}
--- src/com/xupt/mahui/service/ResumeManageService.java
+++ src/com/xupt/mahui/service/ResumeManageService.java
@@ -1,45 +1,11 @@
 package com.xupt.mahui.service;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-
-import com.xupt.mahui.dao.ResumeDao;
-import com.xupt.mahui.entity.ResumeBasic;
-import com.xupt.mahui.util.SqlSessionFactoryUtil;
+/**
+ * 简历管理相关的业务逻辑层的处理
+ * @author mahui
+ *
+ */
 
 public class ResumeManageService {
-	public boolean delete(String phonenumber){
-		SqlSessionFactory sessionFactory=null;
-		SqlSession session=null;
-		try{
-			sessionFactory=SqlSessionFactoryUtil.getSqlSessionFactory();
-			session=sessionFactory.openSession();
-			ResumeDao resumeDao=session.getMapper(ResumeDao.class);
-			
-			resumeDao.deleteProjectExperience(phonenumber);
-			resumeDao.deleteEductionExperience(phonenumber);
-			resumeDao.deleteWorkExperience(phonenumber);
-			resumeDao.deleteResume(phonenumber);
-			session.commit();
-			return true;
-			
-			
-		}catch(Exception e){
-			
-		}finally{
-			session.close();
-		}
-		return false;
-	}
 	
-	public ResumeBasic select(){
-		SqlSessionFactory sessionFactory=null;
-		SqlSession session=null;
-		sessionFactory=SqlSessionFactoryUtil.getSqlSessionFactory();
-		session=sessionFactory.openSession();
-		ResumeDao resumeDao=session.getMapper(ResumeDao.class);
-		session.close();
-		return resumeDao.selectResume("17629034550");	
-		
-	}
 }
