910e8ebd3b3c3a131ecd75ea4d963680c8021b61
ismahui <ismahui@163.com>
2017-06-06 21:29:05 +0800

    add editService

--- src/com/xupt/mahui/controller/ResumeManageController.java
+++ src/com/xupt/mahui/controller/ResumeManageController.java
@@ -2,7 +2,10 @@ package com.xupt.mahui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.xupt.mahui.service.ResumeManageService;
 /**
  * 简历管理相关的Controller
  * @author mahui
@@ -32,10 +35,28 @@ public class ResumeManageController {
 		ModelAndView view=new ModelAndView();
 		return view;
 	}
-	public ModelAndView show(){
+	/**
+	 * 通过电话查询简历的具体信息（即点击编辑的的接口）
+	 * @param phonenumber
+	 * @return
+	 */
+	@RequestMapping("/resumemanage/show")
+	public ModelAndView show(@RequestParam String phonenumber){
 		ModelAndView view=new ModelAndView();
+		/**
+		 * 返回resumeBasic 基本对象
+		 * 返回workExperienceList 集合  
+		 * 返回projectExperienceList 集合  
+		 * 返回eductionExperienceList 集合  
+		 */
+		view.addObject("resumeBasic", ResumeManageService.getResumeBasic(phonenumber));
+		view.addObject("workExperienceList", ResumeManageService.getWorkExperiences(phonenumber));
+		view.addObject("projectExperienceList", ResumeManageService.getWProjectExperiences(phonenumber));
+		view.addObject("eductionExperienceList", ResumeManageService.getEductionExperiences(phonenumber));
 		return view;
 	}
+	
+	
 	@RequestMapping("/type-in")
 	public String test(){
 		return "type-in";
