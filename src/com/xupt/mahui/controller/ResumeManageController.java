b95cbc239aa0c4c6a7d5f78d145b9a50cbda2c2f
ismahui <ismahui@163.com>
2017-06-06 21:16:05 +0800

    delete unuseful thing

--- src/com/xupt/mahui/controller/ResumeManageController.java
+++ src/com/xupt/mahui/controller/ResumeManageController.java
@@ -2,7 +2,6 @@ package com.xupt.mahui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 /**
  * 简历管理相关的Controller
@@ -15,28 +14,19 @@ public class ResumeManageController {
 	public String index(){
 		return "edit";
 	}
-	/**
-	 * search resumes by skill or phonenuber or name from  database
-	 * @return ModelView
-	 */
+	
 	@RequestMapping("/resumemanage/search")
 	public ModelAndView serch(){
 		ModelAndView view=new ModelAndView();
 		return view;
 	}
-	/**
-	 * select one resume from database
-	 * @return ModelView
-	 */
+	
 	@RequestMapping("/resumemanage/select")
 	public ModelAndView select(){
 		ModelAndView view=new ModelAndView();
 		return view;
 	}
-	/**
-	 * insert resume into database
-	 * @return ModelView
-	 */
+	
 	@RequestMapping("/resumemanage/insert")
 	public ModelAndView insert(){
 		ModelAndView view=new ModelAndView();
@@ -47,9 +37,5 @@ public class ResumeManageController {
 	public String test(){
 		return "type-in";
 	}
-	@RequestMapping("/string")
-	@ResponseBody
-	public String string(){
-		return "11";
-	}
+	
 }
